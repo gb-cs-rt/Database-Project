909bf3d54ea611689631390816cc122199bd463d
Baggiio <gustavo.gbaggio@gmail.com>
2024-11-12 13:27:00 -0300

    new document store

--- document_store/main.py
+++ document_store/main.py
@@ -1,188 +1,148 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional
-from bson import ObjectId
-import random
 from pymongo import MongoClient
 from faker import Faker
+import random
 
-fake = Faker('pt_BR')
+# Initialize Faker for generating random data
+fake = Faker("pt_BR")
 
-# Conectar ao MongoDB
+# Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
-db = client['universidade']
+db = client['university']
 
-db.drop_database('universidade')
-# Coleções
-alunos_collection = db['students']
-professores_collection = db['teachers']
-cursos_collection = db['courses']
-grupos_tcc_collection = db['grupos_tcc']
+# Clear existing collections (drop collections if they exist)
+collections_to_drop = [
+    "departamento", "professor", "curso", 
+    "aluno", "disciplina"
+]
+for collection in collections_to_drop:
+    db[collection].drop()
 
-# Cursos e disciplinas
-cursos_e_disciplinas = {
-    "Engenharia de Software": ["Estruturas de Dados", "Sistemas Operacionais", 
-                               "Programação", "Banco de Dados", "Algoritmos"],
-    "Administração": ["Gestão de Pessoas", "Finanças Corporativas", 
-                      "Marketing", "Economia", "Contabilidade"],
-    "Direito": ["Introdução ao Direito", "Direito Penal", 
-                "Direito Civil", "Direito Empresarial", "Direito Constitucional"]
-}
+# Insert data into Departamento
+departamentos = ['Ciência da Computação', 'Engenharia Elétrica', 'Engenharia Mecânica', 'Engenharia de Robôs']
+departamento_docs = [{"nome_departamento": dep} for dep in departamentos]
+departamento_ids = db.departamento.insert_many(departamento_docs).inserted_ids
 
-departamentos = {
-    "Engenharia de Software": "Departamento de Computação",
-    "Administração": "Departamento de Negócios",
-    "Direito": "Departamento de Ciências Jurídicas"
-}
+# Insert data into Professor with embedded "GrupoTCC" and optional "Chefe" field
+professores = []
+for _ in range(40):
+    nome_dep = random.choice(departamentos)
+    professores.append({
+        "nome_departamento": nome_dep,
+        "nome": fake.name(),
+        "email": fake.email(),
+        "telefone": fake.phone_number(),
+        "salario": round(random.uniform(3000, 10000), 2),
+        "grupos_tcc": []  # This will hold GrupoTCC information as a list of group IDs
+    })
+professores_ids = db.professor.insert_many(professores).inserted_ids
 
-class Disciplina(BaseModel):
-    codigo_disciplina: str = Field(max_length=100)
-    nome: str = Field(max_length=100)
-    semestre: int
-    ano: int
-    nota_final: float
-    situacao: str
+# Assign department leaders (ChefeDepartamento)
+for dep_id, dep in zip(departamento_ids, departamentos):
+    id_professor = random.choice(professores_ids)
+    db.professor.update_one({"_id": id_professor}, {"$set": {"chefe_departamento": dep}})
+    db.departamento.update_one({"nome_departamento": dep}, {"$set": {"chefe": id_professor}})
 
-class Curso(BaseModel):
-    id_curso: int
-    nome: str = Field(max_length=100)
-    nome_departamento: str = Field(max_length=100)
-
-class Aluno(BaseModel):
-    ra: int
-    nome: str = Field(max_length=100)
-    email: str = Field(max_length=100)
-    telefone: Optional[str] = Field(max_length=100, default=None)
-    curso: Curso
-    disciplinas: List[Disciplina]
-    formado: bool
-
-class Professor(BaseModel):
-    id: int
-    nome: str = Field(max_length=100)
-    email: str = Field(max_length=100)
-    telefone: Optional[str] = Field(max_length=100)
-    salario: float
-    departamento: str
-    chefe_departamento: bool
-
-class GrupoTCC(BaseModel):
-    id_grupo: int
-    tema: str
-    orientador: int
-    alunos: List[int]
-
-def gerar_nome_aleatorio():
-    return fake.name()
-
-def gerar_disciplina_aleatoria_para_curso(curso_nome):
-    disciplinas = cursos_e_disciplinas.get(curso_nome, ["Disciplina Genérica"])
-    codigo = f"DISC{random.randint(100, 999)}"
-    semestre = random.randint(1, 2)
-    ano = random.randint(2020, 2025)
-    nota_final = round(random.uniform(4, 10), 2)
-    situacao = "Aprovado" if nota_final >= 5.0 else "Reprovado"
-    return Disciplina(
-        codigo_disciplina=codigo,
-        nome=random.choice(disciplinas),
-        semestre=semestre,
-        ano=ano,
-        nota_final=nota_final,
-        situacao=situacao
-    )
-
-def gerar_curso(nome_curso):
-    id_curso = random.randint(1, 1000)
-    curso = Curso(
-        id_curso=id_curso,
-        nome=nome_curso,
-        nome_departamento=departamentos[nome_curso]
-    )
-    cursos_collection.insert_one(curso.dict())
-    return curso
-
-def gerar_aluno_aleatorio():
-    nome_curso = random.choice(list(cursos_e_disciplinas.keys()))
-    curso = cursos_collection.find_one({"nome": nome_curso}) or gerar_curso(nome_curso)
-    disciplinas = [gerar_disciplina_aleatoria_para_curso(nome_curso) for _ in range(5)]
-    aluno = Aluno(
-        ra=random.randint(1000000, 9999999),
-        nome=gerar_nome_aleatorio(),
-        email=fake.email(),
-        telefone=fake.phone_number(),
-        curso=Curso(**curso),
-        disciplinas=disciplinas,
-        formado=random.choice([True, False])
-    )
-    alunos_collection.insert_one(aluno.dict())
-    return aluno
-
-def gerar_professor_aleatorio():
-    nome_curso = random.choice(list(cursos_e_disciplinas.keys()))
-    professor = Professor(
-        id=random.randint(1, 100),
-        nome=gerar_nome_aleatorio(),
-        email=fake.email(),
-        telefone=fake.phone_number(),
-        salario=round(random.uniform(3000, 15000), 2),
-        departamento=departamentos[nome_curso],
-        chefe_departamento=random.choice([True, False])
-    )
-    professores_collection.insert_one(professor.dict())
-    return professor
-
-def gerar_grupo_tcc_aleatorio():
-    id_grupo = random.randint(1, 1000)
-    tema = f"Tema do TCC {id_grupo}"
-    orientador = professores_collection.find_one({}, {"id": 1})["id"]
-    alunos = alunos_collection.aggregate([{"$sample": {"size": 3}}])
-    alunos_ra = [aluno["ra"] for aluno in alunos]
-
-    grupo_tcc = GrupoTCC(
-        id_grupo=id_grupo,
-        tema=tema,
-        orientador=orientador,
-        alunos=alunos_ra
-    )
-    grupos_tcc_collection.insert_one(grupo_tcc.dict())
-    return grupo_tcc
-
-def consultar_grupo_tcc(id_grupo):
-    grupo = grupos_tcc_collection.find_one({"id_grupo": id_grupo})
-    orientador = professores_collection.find_one(
-        {"id": grupo["orientador"]}, {"_id": 0, "nome": 1}
-    )
-    alunos = alunos_collection.find(
-        {"ra": {"$in": grupo["alunos"]}}, {"_id": 0, "nome": 1, "ra": 1}
-    )
-    return {
-        "id_grupo": grupo["id_grupo"],
-        "tema": grupo["tema"],
-        "orientador": orientador["nome"],
-        "alunos": list(alunos)
+# Insert data into Curso with embedded MatrizCurricular
+cursos = []
+for dep in departamentos:
+    curso = {
+        "nome_departamento": dep,
+        "nome": dep,
+        "horas_complementares": random.randint(180, 320),
+        "faltas": random.randint(10, 20),
+        "matriz_curricular": []  # This will hold a list of subjects (Disciplina)
     }
+    cursos.append(curso)
+cursos_ids = db.curso.insert_many(cursos).inserted_ids
 
-def consultar_grupos_por_professor(id_professor):
-    grupos = grupos_tcc_collection.find({"orientador": id_professor}, {"_id": 0, "id_grupo": 1, "tema": 1})
-    return list(grupos)
+# Insert data into Aluno with "GrupoTCC" field
+alunos = []
+for _ in range(100):
+    id_curso = random.choice(cursos_ids)
+    aluno = {
+        "id_curso": id_curso,
+        "nome": fake.name(),
+        "email": fake.email(),
+        "telefone": fake.phone_number(),
+        "grupo_tcc": None  # Will be populated later
+    }
+    alunos.append(aluno)
+alunos_ids = db.aluno.insert_many(alunos).inserted_ids
 
-def consultar_alunos_grupo_tcc(id_grupo):
-    grupo = grupos_tcc_collection.find_one({"id_grupo": id_grupo})
-    alunos = alunos_collection.find(
-        {"ra": {"$in": grupo["alunos"]}}, {"_id": 0, "nome": 1, "email": 1, "telefone": 1}
+# Insert data into Disciplina
+disciplinas = ['Comunicação e Expressão', 'Cálculo I', 'Cálculo II', 'Cálculo III', 'Álgebra Linear', 'Física I', 
+               'Física II', 'Física III', 'Química Geral', 'Química Orgânica', 'Programação I', 'Programação II', 
+               'Programação III', 'Estrutura de Dados', 'Banco de Dados', 'Redes de Computadores', 
+               'Sistemas Operacionais', 'Engenharia de Software', 'Inteligência Artificial', 'Computação Gráfica', 
+               'Sistemas Distribuídos', 'Segurança da Informação', 'Empreendedorismo', 'Gestão de Projetos', 
+               'Tópicos Especiais em Computação', 'Ética e Cidadania', 'Metodologia Científica', 
+               'Trabalho de Conclusão de Curso']
+disciplina_docs = []
+for _ in range(len(disciplinas)):
+    disciplina_docs.append({
+        "codigo_disciplina": fake.unique.bothify(text='??###'),
+        "nome_departamento": random.choice(departamentos),
+        "nome": disciplinas.pop(random.randint(0, len(disciplinas) - 1)),
+        "carga_horaria": random.randint(30, 180)
+    })
+disciplinas_inserted = db.disciplina.insert_many(disciplina_docs).inserted_ids
+
+# Add disciplines to MatrizCurricular of courses
+for curso in db.curso.find():
+    disciplinas_matriz = random.sample(list(db.disciplina.find()), k=random.randint(3, 8))
+    db.curso.update_one({"_id": curso["_id"]}, {"$set": {"matriz_curricular": [d["codigo_disciplina"] for d in disciplinas_matriz]}})
+
+# Insert data into Cursa directly into Aluno collection
+for aluno in db.aluno.find():
+    id_curso = aluno["id_curso"]
+    disciplinas_matriz = list(db.curso.find_one({"_id": id_curso})["matriz_curricular"])
+    cursa = []
+    for disciplina in disciplinas_matriz:
+        cursa.append({
+            "codigo_disciplina": disciplina,
+            "semestre": random.randint(1, 2),
+            "ano": random.randint(2019, 2024),
+            "media": random.uniform(0, 10) if random.random() < 0.3 else random.uniform(5, 10),
+            "faltas": random.randint(0, 10)
+        })
+    db.aluno.update_one({"_id": aluno["_id"]}, {"$set": {"cursa": cursa}})
+
+# Assign GrupoTCC to Aluno and Professor
+qtd_grupos = round(len(alunos) / 3)
+qtd_alunos_por_grupo = 3
+grupos_tcc = []
+for i in range(qtd_grupos):
+    id_grupo = i + 1
+    id_professor = random.choice(professores_ids)
+    grupo_members = []
+    for _ in range(qtd_alunos_por_grupo):
+        ra = alunos_ids.pop(random.randint(0, len(alunos_ids) - 1))
+        grupo_members.append(ra)
+        db.aluno.update_one({"_id": ra}, {"$set": {"grupo_tcc": id_grupo}})
+    db.professor.update_one({"_id": id_professor}, {"$push": {"grupos_tcc": id_grupo}})
+    grupos_tcc.append({
+        "id_grupo": id_grupo,
+        "id_professor": id_professor,
+        "members": grupo_members
+    })
+
+# Insert data into Leciona and embed it in Professor collection
+leciona_docs = []
+for _ in range(80):
+    id_professor = random.choice(professores_ids)
+    id_curso = random.choice(cursos_ids)
+    disciplina = random.choice(list(db.disciplina.find()))
+    leciona_entry = {
+        "id_curso": id_curso,
+        "codigo_disciplina": disciplina["codigo_disciplina"],
+        "semestre": random.randint(1, 2),
+        "ano": random.randint(2019, 2024),
+        "carga_horaria": random.randint(30, 60)
+    }
+    # Add this leciona entry to the corresponding Professor document
+    db.professor.update_one(
+        {"_id": id_professor},
+        {"$push": {"leciona": leciona_entry}}
     )
-    return list(alunos)
 
-# Inserir dados no MongoDB
-for nome_curso in cursos_e_disciplinas.keys():
-    gerar_curso(nome_curso)
-
-for _ in range(10):
-    gerar_aluno_aleatorio()
-
-for _ in range(5):
-    gerar_professor_aleatorio()
-
-for _ in range(3):
-    gerar_grupo_tcc_aleatorio()
-
-print("Dados inseridos com sucesso!")
+print("MongoDB database and collections created with embedded relations!")
